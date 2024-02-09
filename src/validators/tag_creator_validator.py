d94ffe6d732479e1f1ccc3cf731f842fd0a0d84f
ifonso <afonso.script.sol@gmail.com>
2024-02-09 12:10:01 -0400

    fix: adding docs to validator function

--- src/validators/tag_creator_validator.py
+++ src/validators/tag_creator_validator.py
@@ -5,6 +5,22 @@ from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEn
 
 
 def tag_creator_validator(request: Any) -> None:
+    """
+    Validates the incoming request for the tag creator.
+
+    This function checks if the request contains all the necessary fields 
+    and if their values are of the expected types. If the request is valid, 
+    it returns None. If the request is invalid, it raises an appropriate 
+    error.
+
+    Parameters:
+    request (dict): The incoming request to validate (JSON).
+
+    Raises:
+    `HttpUnprocessableEntityError`: If the request is missing necessary fields or the fields 
+    have invalid values.
+    """
+
     schema = {
         "product_code": {
             "type": "string",
